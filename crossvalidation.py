f5971c98b49165faaa0df7cdfa4f92e39ef1f628
aovladi <aovladi@gmail.com>
2017-03-21 09:45:24 -0400

    Added filtered attributes

--- crossvalidation.py
+++ crossvalidation.py
@@ -8,11 +8,13 @@
 ######
 import sys
 import traceback
+import itertools
 import weka.core.jvm as jvm
 from weka.core.converters import Loader
 from weka.classifiers import Classifier, Evaluation, PredictionOutput
 from weka.core.classes import Random
 from weka.filters import Filter, MultiFilter
+from attribute_ranking import rank_attributes
 
 # filtered_attr may be: "1" or "first" - excludes first attribute from dataset ||
 #                       "1-3" - excludes 1,2 and 3 attributes
@@ -40,11 +42,28 @@ def cross_validate(dataset, folds_number, filtered_attr):
     evaluation.crossvalidate_model(classifier,filtered_data, folds_number, Random(65))
 	
     # print statistic
-    print(evaluation.summary())  
-
+    print str(evaluation.summary())
+   
 try:
     jvm.start()
-    cross_validate("housingdata_train.arff", 5, "")
+	    # load ARFF data set
+    loader = Loader(classname='weka.core.converters.ArffLoader')
+    data = loader.load_file('housingdata_train.arff')
+    data.class_is_last()
+
+    # filter string values
+    filter = Filter(classname='weka.filters.unsupervised.attribute.RemoveType',\
+                                                       options=['-T', 'string'])
+    filter.inputformat(data)
+    filtered_data = filter.filter(data)
+	
+    list = list(rank_attributes(filtered_data))
+    list = list[-26:]
+   # for i in xrange(2, 26):
+   # for index, inst in enumerate(itertools.combinations(list, 25)):
+       # attr= ', '.join(map(str, inst))
+    attr = "50, 53, 68, 41, 45, 31, 34, 40, 44, 60, 61, 63, 67, 70, 72"
+    cross_validate("housingdata_train.arff", 5, attr)
 except Exception, e:
     print(traceback.format_exc())
 finally:
